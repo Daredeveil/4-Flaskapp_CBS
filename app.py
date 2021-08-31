e4b4ead80aa73e383c87d68776e8c31f25dd94e8
sabeehraza-83 <sabeehraza@hotmail.com>
2021-08-31 20:07:29 +0200

    Flask

--- app.py
+++ app.py
@@ -5,14 +5,22 @@ import numpy as np
 
 app = Flask(__name__)
 
-model = pickle.load(open('/Users/sabeehraza/Desktop/DS_CA-Berlin/4-Flaskapp_CBS/4-Flaskapp_CBS/00-Ref/model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 day_dict = {'Fri':[1,0,0,0,0,0,0], 'Mon':[0,1,0,0,0,0,0],
             'Sat': [0,0,1,0,0,0,0], 'Sun':[0,0,0,1,0,0,0],
             'Thu':[0,0,0,0,1,0,0], 'Tue':[0,0,0,0,0,1,0],
             'Wed': [0,0,0,0,0,0,1]}
 
-# cols = ['hour', 'is_holiday', 'day_of_week']
+
+month_dict = {'Jan':[1,0,0,0,0,0,0,0,0,0,0,0], 'Feb':[0,1,0,0,0,0,0,0,0,0,0,0],
+            'Mar': [0,0,1,0,0,0,0,0,0,0,0,0], 'Apr':[0,0,0,1,0,0,0,0,0,0,0,0],
+            'May':[0,0,0,0,1,0,0,0,0,0,0,0], 'Jun':[0,0,0,0,0,1,0,0,0,0,0,0],
+            'Jul':[0,0,0,0,0,0,1,0,0,0,0,0], 'Aug':[0,0,0,0,0,0,0,1,0,0,0,0],
+            'Sep':[0,0,0,0,0,0,0,0,1,0,0,0], 'Oct':[0,0,0,0,0,0,0,0,0,1,0,0],
+            'Nov':[0,0,0,0,0,0,0,0,0,0,1,0], 'Dec':[0,0,0,0,0,0,0,0,0,0,0,1],}
+
+# cols = ['hour', 'is_holiday', 'day_of_week', 'month']
 
 @app.route('/')
 def home():
@@ -22,29 +30,10 @@ def home():
 def predict():
     item = [x for x in request.form.values()]
 
-    ## postman begin
-    #hour = request.args.get('hour')
-    #is_holiday = request.args.get('is_holiday')
-    #day_of_week = request.args.get('day_of_week')
-
-    #data = []
-
-    #data.append(hour)
-    #if is_holiday == 'Yes':
-    #    data.extend([0,1])
-    #else:
-    #    data.extend([1,0])
-    #
-    #data.extend(day_dict[day_of_week])
-
-    ### postman end
-
-
-
     data = []
 
     # As the The training data was dummified one, so we have to pass the 
-    # test data in the same format ('hour','is_holiday','day_of_week')
+    # test data in the same format ('hour','is_holiday','day_of_week', 'month')
     
     data.append(int(item[0]))
     
@@ -56,24 +45,15 @@ def predict():
         
     # fri, mon, sat , sun, thu, tue, wed
     data.extend(day_dict[item[2]])
+
+    # (25-08-2021) Jan, Feb, mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+    data.extend(month_dict[item[3]])
     
    
     prediction = int(model.predict([data])[0])
     
-    # postman begin
 
-    # return 'the predicted total bike count :' + str(prediction) 
-
-    # postman end
-   
-
-
-    return render_template('index.html',pred='Total Bike ride counts on {} at {}:00 Hrs will be {}'.format(item[2], item[0],prediction))
-
-
-
-#if __name__ == '__main__':
-#    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
+    return render_template('index.html',pred='Total Bike ride counts in the month of {} on {} at {}:00 Hrs will be {}'.format(item[3], item[2], item[0],prediction))
 
 
 if __name__ == "__main__":
